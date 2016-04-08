9a3c08dc9d4ecf5a7bdcf914a863ca64abcf9425
Victor Julio Gonzalez Rico <vgonzalez@wintrok.com>
2016-04-08 06:28:57 -0500

    Initial Commit

---
+++ PrimerHolaMundoSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrimerHolaMundoSwift
+//
+//  Created by Vijugo on 8/04/16.
+//  Copyright © 2016 nextuniversity.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
